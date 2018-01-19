60e385bd8db328e460df3fd13b63159f77fe8b85
J Carlos Viana F <jcvf@ic.ufal.br>
2018-01-19 17:18:21 -0300

    IssuesSearch class modified

--- search_projects/search/github/issues.py
+++ search_projects/search/github/issues.py
@@ -1,9 +1,18 @@
 from github import Github
 import github_token
+import os
+from time import sleep
 
 class IssuesSearch:
     def run():
         g = Github(github_token.TOKEN)
 
-        for repo in g.get_user().get_repos():
-            print(repo.name)
+#        for repo in g.get_user().get_repos():
+#            print(repo.name)
+        s = g.search_issues("annotation @", "comments", "desc", language="Java", label="bug")
+        '''for issue in s:
+            print(issue.title)
+            print(issue.body)
+            print("---------------------")
+            sleep(10)'''
+        print(s.size)

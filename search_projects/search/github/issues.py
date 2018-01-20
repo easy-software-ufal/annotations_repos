b1365cf7104ae826dfcc724bdb807913ba079a2b
J Carlos Viana F <jcvf@ic.ufal.br>
2018-01-19 23:03:25 -0300

    TODOs created for guide development

--- search_projects/search/github/issues.py
+++ search_projects/search/github/issues.py
@@ -9,10 +9,16 @@ class IssuesSearch:
 
 #        for repo in g.get_user().get_repos():
 #            print(repo.name)
-        s = g.search_issues("annotation @", "comments", "desc", language="Java", label="bug")
+        s = g.search_issues("annotation @", "created", "desc", language="Java", label="bug", state="closed")
         '''for issue in s:
             print(issue.title)
             print(issue.body)
             print("---------------------")
             sleep(10)'''
-        print(s.size)
+        # TODO: for each issue...
+            # TODO: get issue body
+            # TODO: search for annotations words
+            # TODO: catch repo url and count, and issue url (write in json)
+            # TODO: check if project is maven (write in json)
+                # TODO: check if contains dependencies like spring, jboss and others (write in json)
+        # TODO: sumarize projects by order of issues (get json)

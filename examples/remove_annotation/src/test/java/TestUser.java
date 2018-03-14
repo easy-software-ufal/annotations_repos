318ac7abeee6dd472c1205b2eb1956404eaee32b
J Carlos Viana F <jcvf@ic.ufal.br>
2018-03-13 21:46:58 -0300

    basic example using annotation notnull

---
+++ examples/remove_annotation/src/test/java/TestUser.java
@@ -0,0 +1,27 @@
+
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.Test;
+
+public class TestUser {
+
+	@Test
+	public void testAll() {
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+
+		User user = new User();
+		user.setName("Carlos");
+
+		Set<ConstraintViolation<User>> violations = validator.validate(user);
+		assertTrue(violations.isEmpty());
+	}
+}
